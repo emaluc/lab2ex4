7990e2ad17762e22b6c093ae8650ef54aec2bcbc
Emanuela Lucaci <emanuela.lucaci26@gmail.com>
2022-12-06 17:50:27 +0200

    Add project files.

---
+++ lab2ex4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va determina daca un numar este sau nu
+            palindrom.*/
+            Console.WriteLine("Introduceti un numar:");
+
+            int x = int.Parse(Console.ReadLine());
+            int y = x;
+            int reverse = 0;
+            int prov;
+
+            while (y != 0)
+            {
+                prov = y % 10;
+                reverse = reverse * 10 + prov;
+                y = y / 10;
+            }
+            if (x == reverse)
+            {
+                Console.WriteLine("Numarul introdus este palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Numarul introdus nu este palindrom");
+            }
+        }
+    }
+}
